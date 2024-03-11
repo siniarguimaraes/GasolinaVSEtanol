513c8b34202f02305195dc3ad7765e0c9eae5201
Sinair-Guimaraes <siniar_@hotmail.com>
2024-03-11 11:00:03 -0400

    Refatoração do Layout de tela GASOLINA VS ETANOL.

---
+++ app/src/main/java/devandroid/ssg/appgaseta/apoio/UtilGasEta.java
@@ -0,0 +1,20 @@
+package devandroid.ssg.appgaseta.apoio;
+
+public class UtilGasEta {
+
+    public void MetodoEstatico(){
+
+    }
+
+    public static void MetodoNaoEstatico(){
+
+    }
+
+    public static String mensagem(){
+        return "Qualquer Mensagem...";
+    }
+
+    public static double valorRetorno(){
+        return 9.99;
+    }
+}
--- app/src/main/java/devandroid/ssg/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/devandroid/ssg/appgaseta/view/GasEtaActivity.java
@@ -1,18 +1,33 @@
 package devandroid.ssg.appgaseta.view;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.ssg.appgaseta.R;
+import devandroid.ssg.appgaseta.apoio.UtilGasEta;
 
 public class GasEtaActivity extends AppCompatActivity {
+
+    //UtilGasEta utilGasEta = new UtilGasEta();
+    EditText editGasolina;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_gaseta);
+
+        //utilGasEta.MetodoEstatico();
+        //UtilGasEta.MetodoNaoEstatico();
+
+        editGasolina = findViewById(R.id.editGasolina);
+
+        Toast.makeText(GasEtaActivity.this,UtilGasEta.mensagem(),Toast.LENGTH_LONG).show();
+
+
     }
 }
